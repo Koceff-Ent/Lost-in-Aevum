e6cc7a2f346d66fa7577ae3e2a3c95ef279ecaaf
Valentin Vasilev <valio_vv@abv.bg>
2020-11-12 21:26:12 +0200

    Added the pin game object and wrote a script for it

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+       
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (Input.GetKeyDown(KeyCode.E)&& collider.CompareTag("Player"))
+        {
+            //Execution Code
+             Debug.Log("E pressed and collider endered");
+        }
+    }
+
+
+}
