3b888abecbc6d3e7a2f62392ede79a9055c42170
Valentin Vasilev <valio_vv@abv.bg>
2020-11-17 00:04:04 +0200

    Sci fi Laboratory 2 + Canvas to Screen Overlay

--- Assets/Scripts/Pin.cs
+++ Assets/Scripts/Pin.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class Pin : MonoBehaviour
 {
     [SerializeField] GameObject interactionMessage;
-    [SerializeField] GameObject playerController;
-    
+    Vector3 scaleSize = new Vector3(0, 0, 0);
     private void OnTriggerStay(Collider collider)
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-          //  collider.gameObject.SetActive(false);
-            playerController.transform.position.Set(2, 3, 4);
-            
+            // Code to be executed on E pressed, while in range
+
         }
     }
 
@@ -21,12 +19,16 @@ public class Pin : MonoBehaviour
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.CompareTag("Player"))
-            interactionMessage.gameObject.SetActive(true);
+              interactionMessage.gameObject.SetActive(true);
+
     }
 
     private void OnTriggerExit(Collider collider)
     {
+
         if (collider.CompareTag("Player"))
             interactionMessage.gameObject.SetActive(false);
+
+
     }
 }
