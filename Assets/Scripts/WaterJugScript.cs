cccb9a2f8f99c29719a1144c695d13062425d6e1
Valentin Vasilev <valio_vv@abv.bg>
2021-01-19 15:46:49 +0200

    Did Some Water Jug Puzzle Animations

--- Assets/Scripts/WaterJugScript.cs
+++ Assets/Scripts/WaterJugScript.cs
@@ -12,15 +12,33 @@ public class WaterJugScript : MonoBehaviour
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
+            //If a gameObject is hit by the raycast
             if (Physics.Raycast(ray, out hit, 100.0f))
-                if (hit.transform != null && hit.transform.gameObject.name.Equals("MiddleValve"))
+                if (hit.transform != null)
                 {
-                    animatorMiddleValve.Play("ValveSpin", 0, 0.0f);
-                    animatorMiddleValve.Play("BigWaterReduce", 1, 0.0f);
-                    animatorMiddleValve.Play("SmallWaterIncrease", 2, 0.0f);
+                    if (hit.transform.gameObject.name.Equals("MiddleValve"))
+                    {
+                        animatorMiddleValve.Play("ValveSpin", 0, 0.0f);
+                 //       animatorMiddleValve.Play("BigWaterReduce", 1, 0.0f);
+                 //       animatorMiddleValve.Play("SmallWaterIncrease", 2, 0.0f);
+                    }else if (hit.transform.gameObject.name.Equals("LeftValve"))
+                    {
+                        animatorMiddleValve.Play("LeftValveOpen", 4, 0.0f);
+                        animatorMiddleValve.Play("BigWaterReduce", 1, 0.0f);
+                        //Water dropping particle
+                    }else if (hit.transform.gameObject.name.Equals("RightValve"))
+                    {
+                        animatorMiddleValve.Play("RightValveOpen", 5, 0.0f);
+                        animatorMiddleValve.Play("SmallWaterReduce", 3, 0.0f);
+                        //Water dropping particle
+                    }
+
+
                 }
+
         }
+        
+
     }
 
 }
